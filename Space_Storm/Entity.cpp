b4e047f9872eab9938646dfccadfec3e71b022c3
TmCrafz <sterzeltim@gmail.com>
2018-02-06 21:18:07 +0100

    Add Entity class

--- Space_Storm/Entity.cpp
+++ Space_Storm/Entity.cpp
@@ -1,1 +1,18 @@
 #include "Entity.hpp"
+
+Enitiy::Entity()
+: m_pos{0, 0}
+{
+
+}
+
+Vector2 Entity::getPosition()
+{
+    return m_pos;
+}
+
+Vector2 Entity::setPosition(int posX, int posY)
+{
+    m_pos.x = posX;
+    m_pos.y = posY;
+}
--- Space_Storm/Entity.hpp
+++ Space_Storm/Entity.hpp
@@ -1,5 +1,15 @@
+#include "Vector2.hpp"
+
 class Entity
 {
+private:
 
     
+    Vector2 m_pos;
+
+public:
+    Enity();
+
+    Vector2 getPosition();
+    Vector2 setPosition(int posX, int posY);
 };
