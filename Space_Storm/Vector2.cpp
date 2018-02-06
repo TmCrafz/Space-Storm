6e0e9e04bcfae06d3e19f8367de2dcda84ade839
TmCrafz <sterzeltim@gmail.com>
2018-02-06 21:11:29 +0100

    Add Vector class

---
+++ Space_Storm/Vector2.cpp
@@ -0,0 +1,15 @@
+#include "Vector2.hpp"
+
+Vector2::Vector2()
+: x{0}
+, y{0}
+{
+
+}
+Vector2::Vector2(int vecX, int vecY)
+: x{vecX}
+, y{vecY}
+{
+
+}
+
---
+++ Space_Storm/Vector2.hpp
@@ -0,0 +1,10 @@
+class Vector2
+{
+public:
+    int x;
+    int y;
+
+public:
+    Vector2();
+    Vector2(int vecX, int vecY);
+};
