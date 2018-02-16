61948628be9c17296b343524400226d8ef16c22a
TmCrafz <sterzeltim@gmail.com>
2018-02-16 22:02:55 +0100

    Add second background layer

--- Space_Storm/Space_Storm.ino
+++ Space_Storm/Space_Storm.ino
@@ -9,8 +9,13 @@ const unsigned int SCREEN_WIDTH = 128;
 const unsigned int SCREEN_HEIGHT = 64;
 
 Entity player;
-Entity house1;
-const int BG_FAR_AWAY_CNT = 32;
+//Entity house1;
+const int BG_VERY_FAR_AWAY_VEL = -10;
+const int BG_VERY_FAR_AWAY_CNT = 16;
+Entity bgVeryFarAway[BG_VERY_FAR_AWAY_CNT];
+
+const int BG_FAR_AWAY_VEL = -20;
+const int BG_FAR_AWAY_CNT = 16;
 Entity bgFarAway[BG_FAR_AWAY_CNT];
 
 
@@ -43,11 +48,26 @@ float determineDeltaTime()
 void updateBackground(float dt)
 {
     // Move Background
+    for (int i = 0; i < BG_VERY_FAR_AWAY_CNT; i++)
+    {
+        bgVeryFarAway[i].moveBuffered(BG_VERY_FAR_AWAY_VEL * dt, 0);
+        // Check if background is out of screen
+        if (bgVeryFarAway[i].getPosition().x + bgVeryFarAway[i].getSpriteWidth() < 0)
+        {
+            // Out of screen so we move the star outside the screen, so its look like
+            // there is a new star
+            // The new position is in a specific area right outside the screen with has
+            // the same size as the screen
+            int posX = random(SCREEN_WIDTH, SCREEN_WIDTH + SCREEN_WIDTH + 1);
+            int posY = random(0, SCREEN_HEIGHT + 1);
+            bgVeryFarAway[i].setPosition(posX, posY);
+        }
+    }
     for (int i = 0; i < BG_FAR_AWAY_CNT; i++)
     {
-        bgFarAway[i].moveBuffered(-10 * dt, 0);
+        bgFarAway[i].moveBuffered(BG_FAR_AWAY_VEL * dt, 0);
         // Check if background is out of screen
-        if (bgFarAway[i].getPosition().x < 0 + bgFarAway[i].getSpriteWidth())
+        if (bgFarAway[i].getPosition().x + bgFarAway[i].getSpriteWidth() < 0 )
         {
             // Out of screen so we move the star outside the screen, so its look like
             // there is a new star
@@ -88,11 +108,15 @@ void draw()
     arduboy.clear();
     
     // Draw background
+    for (int i = 0; i < BG_VERY_FAR_AWAY_CNT; i++)
+    {
+        bgVeryFarAway[i].draw(arduboy);
+    }
     for (int i = 0; i < BG_FAR_AWAY_CNT; i++)
     {
         bgFarAway[i].draw(arduboy);
     }
-    house1.draw(arduboy);
+    //house1.draw(arduboy);
     player.draw(arduboy);
     // Draw Fps
     arduboy.setCursor(0, 0);
@@ -111,9 +135,9 @@ void setup()
       
     player.setSprite(ImageData::PLAYER_SPRITE, ImageData::PLAYER_SPRITE_WIDTH, 
         ImageData::PLAYER_SPRITE_HEIGHT);
-    house1.setSprite(ImageData::BG_HOUSE_1_SPRITE, ImageData::BG_HOUSE_1_SPRITE_WIDTH, 
-        ImageData::BG_HOUSE_1_SPRITE_HEIGHT);
-    house1.setPosition(0, SCREEN_HEIGHT - ImageData::BG_HOUSE_1_SPRITE_HEIGHT);
+    //house1.setSprite(ImageData::BG_HOUSE_1_SPRITE, ImageData::BG_HOUSE_1_SPRITE_WIDTH, 
+    //    ImageData::BG_HOUSE_1_SPRITE_HEIGHT);
+    //house1.setPosition(0, SCREEN_HEIGHT - ImageData::BG_HOUSE_1_SPRITE_HEIGHT);
     
     
     for (int i = 0; i < BG_FAR_AWAY_CNT; i++)
@@ -121,7 +145,17 @@ void setup()
         int posX = random(0, SCREEN_WIDTH + 1);
         int posY = random(0, SCREEN_HEIGHT + 1);
         bgFarAway[i].setPosition(posX, posY);
-        bgFarAway[i].setSprite(ImageData::BG_STAR_VERY_FAR_SPRITE, 
+        bgFarAway[i].setSprite(ImageData::BG_STAR_FAR_SPRITE, 
+            ImageData::BG_STAR_FAR_SPRITE_WIDTH, 
+            ImageData::BG_STAR_FAR_SPRITE_HEIGHT
+        );
+    }
+    for (int i = 0; i < BG_VERY_FAR_AWAY_CNT; i++)
+    {
+        int posX = random(0, SCREEN_WIDTH + 1);
+        int posY = random(0, SCREEN_HEIGHT + 1);
+        bgVeryFarAway[i].setPosition(posX, posY);
+        bgVeryFarAway[i].setSprite(ImageData::BG_STAR_VERY_FAR_SPRITE, 
             ImageData::BG_STAR_VERY_FAR_SPRITE_WIDTH, 
             ImageData::BG_STAR_VERY_FAR_SPRITE_HEIGHT
         );
